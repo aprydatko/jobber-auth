cc5dda92066e517955ffdd42de5c6594112c4e22
Artur Prydatko <artyrpridatko@gmail.com>
2025-06-14 22:09:35 +0300

    added seeding controller and routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,16 +2,19 @@ import { Application } from 'express';
 import { authRoutes } from '@auth/routes/auth';
 import { verifyGatewayRequest } from '@aprydatko/jobber-shared';
 import { currentUserRoutes } from '@auth/routes/current-user';
-
-import { healthRoutes } from './routes/health';
+import { healthRoutes } from '@auth/routes/health';
+import { searchRoutes } from '@auth/routes/search';
+import { seedRoutes } from '@auth/routes/seed';
 
 const BASE_PATH = '/api/v1/auth';
 
 export function appRoutes(app: Application): void {
   app.use('', healthRoutes());
+  app.use(BASE_PATH, searchRoutes());
+  app.use(BASE_PATH, seedRoutes());
 
- app.use(BASE_PATH, verifyGatewayRequest, authRoutes());
- app.use(BASE_PATH, verifyGatewayRequest, currentUserRoutes());
+  app.use(BASE_PATH, verifyGatewayRequest, authRoutes());
+  app.use(BASE_PATH, verifyGatewayRequest, currentUserRoutes());
 }
 
 
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import cors from 'cors';
 import { Logger } from 'winston';
 import { verify } from 'jsonwebtoken';
 import compression from 'compression';
-import { checkConnection } from '@auth/elasticsearch';
+import { checkConnection, createIndex } from '@auth/elasticsearch';
 import { appRoutes } from '@auth/routes';
 import { Channel } from 'amqplib';
 import { createConnection } from '@auth/queues/connection';
@@ -66,6 +66,7 @@ async function startQueues(): Promise<void> {
 
 function startElasticSearch(): void {
   checkConnection();
+  createIndex('gigs');
 }
 
 function authErrorHandler(app: Application): void {
